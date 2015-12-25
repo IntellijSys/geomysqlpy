192b350d828538b4e050f1d1e1f1858657e56871
Mohammad Nurdin (Zero) <mohammadnrdn@gmail.com>
2015-12-26 00:45:51 +0800

    Add mysql functionality.

--- geocode_converter.py
+++ geocode_converter.py
@@ -1,3 +1,21 @@
-import MySQLdb
+import MySQLdb as mdb
 import sys
+from geopy.geocoders import Nominatim
 
+db = None
+
+try:
+
+    db = mdb.connect("localhost","root", "root", "your_database")
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM your_table")
+    rows = cur.fetchall()
+    for row in rows:
+    	geolocator = Nominatim()
+        print row
+
+except mdb.Error, e:
+
+    print "Error %d: %s" % (e.args[0],e.args[1])
+    sys.exit(1)
